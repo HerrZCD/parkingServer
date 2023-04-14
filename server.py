aa5d8de5a2b5fb156148e6167767f2f81c4dc0cd
Chundong Zhang <chuzhang@microsoft.com>
2023-04-15 00:00:00 +0800

    server

--- server.py
+++ server.py
@@ -74,6 +74,57 @@ def HandleRegister():
     response.headers['Access-Control-Allow-Headers'] = 'x-requested-with'
     return response
 
+@app.route("/addspots", methods=['GET', 'POST'])
+def HandleRegister():
+    data = request.get_data(as_text=True);
+    j_data = json.loads(data);
+    print(data)
+    print(j_data)
+    print(j_data['width'])
+    print(j_data['height'])
+    print(j_data['location'])
+    print(j_data['price'])
+    print(j_data['user_time_start'])
+    print(j_data['user_time_end'])
+
+    width = j_data['width'];
+    height = j_data['height'];
+    location = j_data['location'];
+    price = j_data['price'];
+    user_time_start = j_data['user_time_start'];
+    user_time_end = j_data['user_time_end'];
+
+    if width and height and location:
+        try:
+            sql = 'INSERT INTO spots (width, height, location, price, user_time_start, user_time_end) values ("{width}", "{height}", "{location}", "{price}", "{user_time_start}", "{user_time_end}");'.format(width=width, height=height, location=location, price=price, user_time_start=user_time_start, user_time_end=user_time_end);
+            print(sql)
+            cursor.execute(sql);
+            db.commit();
+            result_text = {"statusCode": 200, "status": "success"}
+        except:
+            result_text = {"statusCode": 200, "status": "fail"}
+        response = make_response(jsonify(result_text))
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'OPTIONS,HEAD,GET,POST'
+        response.headers['Access-Control-Allow-Headers'] = 'x-requested-with'
+        return response
+
+def EnsureParkingSpotsTable():
+    sql = '''
+    CREATE TABLE IF NOT EXISTS `spots`(
+   `id` INT UNSIGNED AUTO_INCREMENT,
+   `width` INT NOT NULL,
+   `height` INT NOT NULL,
+   `location` varchar(100) NOT NULL,
+   `user_time_start` INT,
+   `user_time_end` INT,
+   `price` INT,
+    PRIMARY KEY ( `id` )
+    )
+    '''
+    cursor.execute(sql);
+
+EnsureParkingSpotsTable();
 
 
  
